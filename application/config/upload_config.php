a42388a063df6fdb9b95330bc6b871e4b345391c
Kamrul Hasan <saj696@gmail.com>
2015-01-07 16:18:12 +0600

    upload config modified

--- application/config/upload_config.php
+++ application/config/upload_config.php
@@ -3,8 +3,13 @@
 $config['fifteen_days_picture_report_img_size']= 2000000;
 $config['fifteen_days_picture_report_img_type']= 'image';
 $config['fifteen_days_picture_report_img_upload_folder']= FCPATH.'images/trail_fiftyfive_picture_report';
-
 $config['fifteen_days_picture_report_img_allowed_types']= 'jpg|jpeg|png';
+
+
+$config['flowering_picture_report_img_size']= 2000000;
+$config['flowering_picture_report_img_type']= 'image';
+$config['flowering_picture_report_img_upload_folder']= FCPATH.'images/trail_fiftyfive_picture_report';
+$config['flowering_picture_report_img_allowed_types']= 'jpg|jpeg|png';
 
 ///  Trail Fruit Picture Entry
 
@@ -13,9 +18,14 @@ $config['fruit_picture_report_img_size']= 2000000;
 $config['fruit_picture_report_img_type']= 'image';
 $config['fruit_picture_report_after_harvesting_img_upload_folder']= FCPATH.'images/trial_fruit_picture_entry/after_harvesting';
 $config['fruit_picture_report_before_harvesting_img_upload_folder']= FCPATH.'images/trial_fruit_picture_entry/before_harvesting';
-
 $config['fruit_picture_report_img_allowed_types']= 'jpg|jpeg|png';
 
+$config['fruit_picture_report_img_size']= 2000000;
+$config['fruit_picture_report_img_type']= 'image';
+$config['fruit_picture_report_after_harvesting_img_upload_folder']= FCPATH.'images/trial_fruit_picture_entry/after_harvesting';
+$config['fruit_picture_report_before_harvesting_img_upload_folder']= FCPATH.'images/trial_fruit_picture_entry/before_harvesting';
+
+$config['fruit_picture_report_img_allowed_types']= 'jpg|jpeg|png';
 
 
 
