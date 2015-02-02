e2e8c67fa8f0181e3019776e97423a7b60896814
Kamrul Hasan <saj696@gmail.com>
2015-02-02 16:26:42 +0600

    Config_fifteen_days modified

--- application/controllers/config_fifteen_days.php
+++ application/controllers/config_fifteen_days.php
@@ -47,12 +47,11 @@ class Config_fifteen_days extends ROOT_Controller
 
     public function rnd_list($id)
     {
-        
+
     }
 
     public function rnd_save()
     {
-
         $id = $this->input->post("crop_id");
         $user = User_helper::get_user();
 
@@ -76,7 +75,6 @@ class Config_fifteen_days extends ROOT_Controller
         }
         else
         {
-
             $this->db->trans_start();  //DB Transaction Handle START
             $time=time();
 
@@ -84,15 +82,7 @@ class Config_fifteen_days extends ROOT_Controller
             $data['creation_date'] = $time;
 
             $crop_id=Query_helper::add('rnd_crop',$data);
-            $status_data['crop_id']=$crop_id;
-            $status_data['created_by']=$user->user_id;
-            $status_data['creation_date']=$time;
-            Query_helper::add('rnd_status_fifteen_days_report',$status_data);
-            Query_helper::add('rnd_status_flowering_report',$status_data);
-            Query_helper::add('rnd_status_fruit_report',$status_data);
-            Query_helper::add('rnd_status_harvest_compile_report',$status_data);
-            Query_helper::add('rnd_status_harvest_report',$status_data);
-            Query_helper::add('rnd_status_yield_report',$status_data);
+
             $this->db->trans_complete();   //DB Transaction Handle END
 
             if ($this->db->trans_status() === TRUE)
@@ -104,95 +94,15 @@ class Config_fifteen_days extends ROOT_Controller
                 $this->message=$this->lang->line("MSG_NOT_SAVED_SUCCESS");
             }
 
-            $this->rnd_list();//this is similar like redirect
+            $this->rnd_add_edit();//this is similar like redirect
         }
 
     }
-    public function rnd_save_ordering()
-    {
 
-        $id = $this->input->post("crop_id");
-        $data['ordering']=$this->input->post("ordering");
-        $user = User_helper::get_user();
-        $this->db->trans_start();  //DB Transaction Handle START
-
-        $data['modified_by'] = $user->user_id;
-        $data['modification_date'] = time();
-
-        Query_helper::update('rnd_crop',$data,array("id = ".$id));
-
-        $this->db->trans_complete();   //DB Transaction Handle END
-
-        if ($this->db->trans_status() === TRUE)
-        {
-            $this->message=$this->lang->line("MSG_UPDATE_SUCCESS");
-        }
-        else
-        {
-            $this->message=$this->lang->line("MSG_NOT_UPDATED_SUCCESS");
-        }
-        $this->rnd_list();//this is similar like redirect
-
-    }
 
     private function check_validation()
     {
-        $valid=true;
-        if(Validation_helper::validate_empty($this->input->post('crop_name')))
-        {
-            $valid=false;
-            $this->message.="Crop Name Cannot Be Empty<br>";
-        }
-        elseif($this->create_crop_model->check_crop_name_existence($this->input->post('crop_name')))
-        {
-            $valid=false;
-            $this->message.="Crop Name Exists<br>";
-        }
 
-        if(Validation_helper::validate_empty($this->input->post('crop_code')))
-        {
-            $valid=false;
-            $this->message.="Crop Code Cannot Be Empty<br>";
-        }
-        elseif($this->create_crop_model->check_crop_code_existence($this->input->post('crop_code')))
-        {
-            $valid=false;
-            $this->message.="Crop Code Exists<br>";
-        }
-        if(!Validation_helper::validate_numeric($this->input->post('crop_width')))
-        {
-            $valid=false;
-            $this->message.="Crop Width Must be number<br>";
-        }
-        if(!Validation_helper::validate_numeric($this->input->post('crop_height')))
-        {
-            $valid=false;
-            $this->message.="Crop height Must be number<br>";
-        }
-        if(!Validation_helper::validate_int($this->input->post('fruit_type')))
-        {
-            $valid=false;
-            $this->message.="Select Fruit Type<br>";
-        }
-
-        if(!Validation_helper::validate_numeric($this->input->post('sample_size')))
-        {
-            $valid=false;
-            $this->message.="Sample size Must be number<br>";
-        }
-
-        if(!Validation_helper::validate_int($this->input->post('initial_plants')))
-        {
-            $valid=false;
-            $this->message.="Initial plants Must be number<br>";
-        }
-        if(!Validation_helper::validate_int($this->input->post('plants_per_hectare')))
-        {
-            $valid=false;
-            $this->message.="Plants per hectare Must be number<br>";
-        }
-
-        return $valid;
     }
 
 
