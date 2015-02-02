998f5892c0e25ba92a4d20489f5a7b0a71d87cc5
Kamrul Hasan <saj696@gmail.com>
2015-02-02 16:54:54 +0600

    config 15 days back button hide

--- application/views/action_buttons_edit.php
+++ application/views/action_buttons_edit.php
@@ -6,7 +6,7 @@
     <div class="action_button" style="display: <?php if(isset($hide_save)&&($hide_save==1)){ echo "none";} else{echo "block";} ?>">
         <button id="button_save" class="btn"><?php echo $this->lang->line("ACTION_SAVE"); ?></button>
     </div>
-    <div class="action_button">
+    <div class="action_button" style="display: <?php if(isset($hide_back)&&($hide_back==1)){ echo "none";} else{echo "block";} ?>">
         <a class="btn" href="<?php echo $link_back; ?>"><?php echo $this->lang->line("ACTION_BACK"); ?></a>
     </div>
     <div class="action_button">
