aaa919c0479b12099cd3983c803c753046ea2e34
Kamrul Hasan <saj696@gmail.com>
2015-03-07 18:32:51 +0600

    fruit config modified

--- application/config/fruit_config.php
+++ application/config/fruit_config.php
@@ -12,56 +12,56 @@ $config['fc_fruit_shape_4'][3]='Sq. Round';
 $config['fc_fruit_shape_4'][4]='Roma';
 
 // fc_fruit Shape Brinjal
-$config['fc_fruit_shape_6'][1]='Long';
-$config['fc_fruit_shape_6'][2]='Oval';
-$config['fc_fruit_shape_6'][3]='Round';
+$config['fc_fruit_shape_5'][1]='Long';
+$config['fc_fruit_shape_5'][2]='Oval';
+$config['fc_fruit_shape_5'][3]='Round';
 
 // fc_fruit Shape Chili
-$config['fc_fruit_shape_5'][1]='Short';
-$config['fc_fruit_shape_5'][2]='Medium';
-$config['fc_fruit_shape_5'][3]='Long';
+$config['fc_fruit_shape_6'][1]='Short';
+$config['fc_fruit_shape_6'][2]='Medium';
+$config['fc_fruit_shape_6'][3]='Long';
 
 // fc_fruit Shape Bitter Gourd
-$config['fc_fruit_shape_8'][1]='Short';
-$config['fc_fruit_shape_8'][2]='Medium';
-$config['fc_fruit_shape_8'][3]='Long';
+$config['fc_fruit_shape_21'][1]='Short';
+$config['fc_fruit_shape_21'][2]='Medium';
+$config['fc_fruit_shape_21'][3]='Long';
 
 // fc_fruit Shape Ridge Gourd
-$config['fc_fruit_shape_11'][1]='Short';
-$config['fc_fruit_shape_11'][2]='Medium';
-$config['fc_fruit_shape_11'][3]='Long';
+$config['fc_fruit_shape_22'][1]='Short';
+$config['fc_fruit_shape_22'][2]='Medium';
+$config['fc_fruit_shape_22'][3]='Long';
 
 // fc_fruit Shape Snake Gourd
-$config['fc_fruit_shape_12'][1]='Short';
-$config['fc_fruit_shape_12'][2]='Medium';
-$config['fc_fruit_shape_12'][3]='Long';
+$config['fc_fruit_shape_20'][1]='Short';
+$config['fc_fruit_shape_20'][2]='Medium';
+$config['fc_fruit_shape_20'][3]='Long';
 
 // fc_fruit Shape Sponge Gourd
-$config['fc_fruit_shape_14'][1]='Short';
-$config['fc_fruit_shape_14'][2]='Medium';
-$config['fc_fruit_shape_14'][3]='Long';
+$config['fc_fruit_shape_19'][1]='Short';
+$config['fc_fruit_shape_19'][2]='Medium';
+$config['fc_fruit_shape_19'][3]='Long';
 
 // fc_fruit Shape Cucumber
-$config['fc_fruit_shape_7'][1]='Cylindrical';
-$config['fc_fruit_shape_7'][2]='Narrow';
+$config['fc_fruit_shape_13'][1]='Cylindrical';
+$config['fc_fruit_shape_13'][2]='Narrow';
 
 // fc_fruit Shape Bottle Gourd
-$config['fc_fruit_shape_9'][1]='Bottle';
-$config['fc_fruit_shape_9'][2]='Cylindrical';
+$config['fc_fruit_shape_18'][1]='Bottle';
+$config['fc_fruit_shape_18'][2]='Cylindrical';
 
 // fc_fruit Shape Pumpkin
-$config['fc_fruit_shape_10'][1]='Flat';
-$config['fc_fruit_shape_10'][2]='Round';
-$config['fc_fruit_shape_10'][3]='Oblong';
+$config['fc_fruit_shape_24'][1]='Flat';
+$config['fc_fruit_shape_24'][2]='Round';
+$config['fc_fruit_shape_24'][3]='Oblong';
 
 // fc_fruit Shape Wax
-$config['fc_fruit_shape_13'][1]='Cylindrical';
-$config['fc_fruit_shape_13'][2]='Roundish';
+$config['fc_fruit_shape_23'][1]='Cylindrical';
+$config['fc_fruit_shape_23'][2]='Roundish';
 
 // fc_fruit Shape Water melon
-$config['fc_fruit_shape_18'][1]='Oblong';
-$config['fc_fruit_shape_18'][2]='Oval';
-$config['fc_fruit_shape_18'][3]='Round';
+$config['fc_fruit_shape_25'][1]='Oblong';
+$config['fc_fruit_shape_25'][2]='Oval';
+$config['fc_fruit_shape_25'][3]='Round';
 
 
 // fc_fruit Colour Tomato
@@ -70,53 +70,53 @@ $config['fc_fruit_colour_4'][2]='Orange';
 $config['fc_fruit_colour_4'][3]='Pink';
 
 // fc_fruit Colour Chilli
-$config['fc_fruit_colour_5'][1]='Deep Green';
-$config['fc_fruit_colour_5'][2]='Green';
-$config['fc_fruit_colour_5'][3]='Light Green';
+$config['fc_fruit_colour_6'][1]='Deep Green';
+$config['fc_fruit_colour_6'][2]='Green';
+$config['fc_fruit_colour_6'][3]='Light Green';
 
 // fc_fruit Colour Brinjal
-$config['fc_fruit_colour_6'][1]='Purple';
-$config['fc_fruit_colour_6'][2]='Green';
-$config['fc_fruit_colour_6'][3]='Black';
+$config['fc_fruit_colour_5'][1]='Purple';
+$config['fc_fruit_colour_5'][2]='Green';
+$config['fc_fruit_colour_5'][3]='Black';
 
 // fc_fruit Colour Cucumber
-$config['fc_fruit_colour_7'][1]='Green';
-$config['fc_fruit_colour_7'][2]='Light Green';
-$config['fc_fruit_colour_7'][3]='Whitish';
+$config['fc_fruit_colour_13'][1]='Green';
+$config['fc_fruit_colour_13'][2]='Light Green';
+$config['fc_fruit_colour_13'][3]='Whitish';
 
 // fc_fruit Colour Bitter Gourd
-$config['fc_fruit_colour_8'][1]='Light Green';
-$config['fc_fruit_colour_8'][2]='Green';
-$config['fc_fruit_colour_8'][3]='Deep Green';
+$config['fc_fruit_colour_21'][1]='Light Green';
+$config['fc_fruit_colour_21'][2]='Green';
+$config['fc_fruit_colour_21'][3]='Deep Green';
 
 // fc_fruit Colour Bottle Gourd
-$config['fc_fruit_colour_9'][1]='Light Green';
-$config['fc_fruit_colour_9'][2]='Deep Green';
+$config['fc_fruit_colour_18'][1]='Light Green';
+$config['fc_fruit_colour_18'][2]='Deep Green';
 
 // fc_fruit Colour Pumpkin
-$config['fc_fruit_colour_10'][1]='White Spotted';
-$config['fc_fruit_colour_10'][2]='Deep Green';
+$config['fc_fruit_colour_24'][1]='White Spotted';
+$config['fc_fruit_colour_24'][2]='Deep Green';
 
 // fc_fruit Colour Ridge Gourd
-$config['fc_fruit_colour_11'][1]='Light Green';
-$config['fc_fruit_colour_11'][2]='Green';
-$config['fc_fruit_colour_11'][3]='Deep Green';
+$config['fc_fruit_colour_22'][1]='Light Green';
+$config['fc_fruit_colour_22'][2]='Green';
+$config['fc_fruit_colour_22'][3]='Deep Green';
 
 // fc_fruit Colour Okra
-$config['fc_fruit_colour_17'][1]='Light Green';
-$config['fc_fruit_colour_17'][2]='Green';
+$config['fc_fruit_colour_14'][1]='Light Green';
+$config['fc_fruit_colour_14'][2]='Green';
 
 // fc_fruit Colour Snake Gourd
-$config['fc_fruit_colour_12'][1]='Deep Green';
-$config['fc_fruit_colour_12'][2]='Light Green';
+$config['fc_fruit_colour_20'][1]='Deep Green';
+$config['fc_fruit_colour_20'][2]='Light Green';
 
 // fc_fruit Colour Sponge Gourd
-$config['fc_fruit_colour_14'][1]='Deep Green';
-$config['fc_fruit_colour_14'][2]='Light Green';
+$config['fc_fruit_colour_19'][1]='Deep Green';
+$config['fc_fruit_colour_19'][2]='Light Green';
 
 // fc_fruit Colour Wax Gourd
-$config['fc_fruit_colour_13'][1]='Deep Green';
-$config['fc_fruit_colour_13'][2]='Light Green';
+$config['fc_fruit_colour_23'][1]='Deep Green';
+$config['fc_fruit_colour_23'][2]='Light Green';
 
 // fc_fruit Taste
 $config['fc_fruit_taste'][1]='Pungent';
@@ -140,9 +140,9 @@ $config['fc_curd_colour_1'][2]='Creamy white';
 $config['fc_curd_colour_1'][3]='White';
 
 // fc_curd Colour Broccoli
-$config['fc_curd_colour_20'][1]='Deep Green';
-$config['fc_curd_colour_20'][2]='Green';
-$config['fc_curd_colour_20'][3]='Light Green';
+$config['fc_curd_colour_7'][1]='Deep Green';
+$config['fc_curd_colour_7'][2]='Green';
+$config['fc_curd_colour_7'][3]='Light Green';
 
 // Head Type
 $config['fc_head_type'][1]='Ball';
@@ -170,14 +170,14 @@ $config['fc_root_colour_15'][2]='Green Nack';
 $config['fc_root_colour_15'][3]='Pink';
 
 // Root Color Turnip
-$config['fc_root_colour_22'][1]='Purple';
-$config['fc_root_colour_22'][2]='Greenish';
-$config['fc_root_colour_22'][3]='White';
+$config['fc_root_colour_10'][1]='Purple';
+$config['fc_root_colour_10'][2]='Greenish';
+$config['fc_root_colour_10'][3]='White';
 
 // Root Color Carrot
-$config['fc_root_colour_16'][1]='Orange';
-$config['fc_root_colour_16'][2]='Light Orange';
-$config['fc_root_colour_16'][3]='Others';
+$config['fc_root_colour_17'][1]='Orange';
+$config['fc_root_colour_17'][2]='Light Orange';
+$config['fc_root_colour_17'][3]='Others';
 
 // Root Taste
 $config['fc_root_taste'][1]='Pungent';
@@ -193,8 +193,8 @@ $config['fc_spine_type'][2]='Semi Blunt';
 $config['fc_spine_type'][3]='Pointed';
 
 // Flesh Colour Pumpkin
-$config['fc_flesh_colour_10'][1]='Red';
-$config['fc_flesh_colour_10'][2]='Yellow';
+$config['fc_flesh_colour_24'][1]='Red';
+$config['fc_flesh_colour_24'][2]='Yellow';
 
 // Ridge Type
 $config['fc_ridge_type'][1]='Ridged';
